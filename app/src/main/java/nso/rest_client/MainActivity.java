88a4403bb0398eb9b38546d3f8e830c8cd408614
koreedanaoki <koreedanaoki@MacBook-Pro-No-2.local>
2016-06-14 18:47:27 +0900

    Adding onClick event

--- app/src/main/java/nso/rest_client/MainActivity.java
+++ app/src/main/java/nso/rest_client/MainActivity.java
@@ -2,6 +2,9 @@ package nso.rest_client;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView txtRequestUri = (TextView)findViewById(R.id.txtRequestUri);
+        Button btnExecute = (Button)findViewById(R.id.btnExecute);
+
+        final TextView txtViewRespose = (TextView)findViewById(R.id.txtViewResponse);
+
+        btnExecute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence strUri = txtRequestUri.getText();
+
+                txtViewRespose.setText(strUri);
+            }
+        });
+
     }
 }
